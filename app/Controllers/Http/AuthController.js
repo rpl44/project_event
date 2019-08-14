7a6575a7831b5bc7ff11c0efbb55c85b222aba4e
Hilmi Hilmi <hilmi.kyosou.080202@gmail.com>
2019-08-14 13:41:03 +0700

    AuthController Edit

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,9 +1,22 @@
 'use strict'
 
-const account = use('App/Models/Account')
+const Hash = use('Hash')
+const Account = use('App/Models/Account')
+const Address = use('App/Models/Address')
+const AuthToken = use('App/Models/AuthToken')
 const {validate} = use('Validator')
 
 class AuthController {
+   
+    randomString(string_length){
+        var result = "";
+        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        for(var i = 0; i<string_length; i++){
+            result += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+        return result
+    }
+    
     async add_account({request,session,response}){
         
         //validation
@@ -19,7 +32,28 @@ class AuthController {
             return response.redirect('back')
         }
         //endvalidation
-        //data-to-database
+
+        //address data
+        new_account_data = request.all()
+        const address = new Address()
+        await address.save()
+        
+        //account data
+        const account = new Account()
+        account.name = new_account_data.in_name
+        account.username = new_account_data.in_username
+        account.password = new_account_data.in_password
+        account.address_id = address.id
+        await account.save()
+        //enddata-to-database
+        
+        console.log("a new account has been added")
+        session.flash({ notification: 'Your account have been successfully registered' })
+        return response.redirect('back')
+        
+    }
+
+    async account_login({}){
         
     }
 }
